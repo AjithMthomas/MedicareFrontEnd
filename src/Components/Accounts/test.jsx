19b1c2e15e0045d1fbd3e691cdf12971042ba7c8
AjithMthomas <ajithmt46958@gmail.com>
2023-07-12 20:26:03 +0530

    changes

--- src/Components/Accounts/test.jsx
+++ src/Components/Accounts/test.jsx
@@ -118,25 +118,8 @@ function SignUp() {
                   {errors.password && <div className="text-red-500 text-start">{errors.password.message}</div>}
                 </div>
               </div>
-              <div className="mb-2">
-                <label htmlFor="confirm_pass" className="block text-sm font-semibold text-gray-800">
-                  Confirm Password
-                </label>
-                <input
-                  type="password"
-                  name="confirm_pass"
-                  {...register('confirm_pass')}
-                  className="w-3/4 px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-                <div className="w-3/4 ms-16">
-                  {errors.confirm_pass && <div className="text-red-500 text-start">{errors.confirm_pass.message}</div>}
-                </div>
-              </div>
-              <div className="mt-6">
-                <button className="w-80 px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-                  Register
-                </button>
-              </div>
+           
+             
             </form>
             <p className="mt-8 text-xs font-light text-center text-gray-700">
               Have an account?{" "}
