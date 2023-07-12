1eb651203eb3b0294338969d3ddba44ca176c27a
AjithMthomas <ajithmt46958@gmail.com>
2023-07-12 19:25:25 +0530

    changes

--- src/Components/Contexts/auth.js
+++ src/Components/Contexts/auth.js
@@ -5,7 +5,7 @@ export default async function login(e) {
   console.log( {'email': e.target.username.value,
   'password': e.target.password.value})
   try {
-    const response = await fetch('http://127.0.0.1:8000/api/login/', {
+    const response = await fetch('https://medicarebackends.onrender.com/api/login/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
